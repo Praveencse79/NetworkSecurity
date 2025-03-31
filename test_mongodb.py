545f07b7e9b3c82a2d55ae9d48d1b4b92449cbe9
Praveencse79 <praveenhelp79@gmail.com>
2025-03-31 19:47:52 +0530

    changing the user name & Pass

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://praveenkrcse:<@password>@cluster0.wnqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://praveencseds:Admin123@cluster0.ylx2nml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
